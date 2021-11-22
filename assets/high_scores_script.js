d4bd0b9df3199a7350df99ba4fbeff9f73fb6be7
chriskimball <92878250+chriskimball@users.noreply.github.com>
2021-11-22 00:47:59 -0800

    Added clear leaderboard and local storage added. Also added array method on initial page load that will sort the incoming local storage leaderboard array.

--- assets/high_scores_script.js
+++ assets/high_scores_script.js
@@ -9,6 +9,13 @@ function init() {
     // TODO: What is the purpose of the following line of code?
     // this will parse the todo array from local storage from string into an array
     leaderboard = JSON.parse(localStorage.getItem("leaderboard"));
+    
+    leaderboard.sort(function(a, b){
+        var scoreA= a.totalScore;
+        var scoreB= b.totalScore;
+    return scoreB-scoreA
+    })
+    console.log(leaderboard)
     // TODO: Describe the functionality of the following `if` statement.
     // if local storage is not blank then it will set the stored todos to the todos variable
     if (leaderboard !== null) {
@@ -33,21 +40,13 @@ function backToWelcome(event) {
     window.location.href='index.html'
 }
 
-backBtnEl.addEventListener( "click" , backToWelcome)
+backBtnEl.addEventListener( "click" , backToWelcome )
+clearBtnEl.addEventListener( "click" , clearLeaderboard )
 
 
 // TODO: render leaderboard upon submission and leaderboard page load
 function renderLeaderboard() {
-    // TODO: Describe the functionality of the following two lines of code.
-    // defines the todo list's inner html as string values
-    // increments the number of items on your todo list to equal the todo array length
-    
-    console.log(leaderboard.length)
-    // todoCountSpan.textContent = leaderboard.length;
-    
-    // TODO: Describe the functionality of the following `for` loop.
-    // converts todo array into list items on the todo list
-    
+    htmlTemplateString = "";
     for(var i=0; i < leaderboard.length; i++) {
 
         //back ticks for template literals is key
@@ -57,14 +56,19 @@ function renderLeaderboard() {
                 <td>${leaderboard[i].timeLeft}</td>
                 <td>${leaderboard[i].answeredCorrectly}</td>
                 <td>${leaderboard[i].totalScore}</td>
-            </tr>`;
-            
+            </tr>`; 
     }
     leaderboardEl.innerHTML += htmlTemplateString
 }
     
 
 // TODO: clear out the leaderboard from local storage
-// function clearLeaderboard () {
+function clearLeaderboard () {
+    leaderboard = []
+    leaderboardEl.children[1].innerHTML = "";
+    console.log(leaderboard)
     
-// }
+    localStorage.setItem("leaderboard", JSON.stringify(leaderboard))
+    console.log(leaderboard)
+    renderLeaderboard()
+}
