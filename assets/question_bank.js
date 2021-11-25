70b2703a9d92c264156e794f64e8c2462bb5cc5d
chriskimball <92878250+chriskimball@users.noreply.github.com>
2021-11-25 01:29:33 -0800

    Expanded question bank to 24 questions

--- assets/question_bank.js
+++ assets/question_bank.js
@@ -1,7 +1,7 @@
 // Array of Questions
 var questions = [
     {   
-        
+
         questionName: "What color is the sky?",
         answerOptions: [
             "Blue",
@@ -24,10 +24,10 @@ var questions = [
     {
         questionName: "How many people have landed on the moon?",
         answerOptions: [
-            "1",
+            "12",
             "2",
             "3",
-            "12"
+            "122"
         ],
         correctAnswer: "12"
     },
@@ -54,13 +54,196 @@ var questions = [
     {
         questionName: "How many people have landed on the moon?",
         answerOptions: [
-            "1",
+            "12",
             "2",
             "3",
-            "12"
+            "122"
         ],
         correctAnswer: "12"
-    }
+    },
+    {   
+
+        questionName: "What color is the sky?",
+        answerOptions: [
+            "Blue",
+            "Red",
+            "Alert",
+            "Number"
+        ],
+        correctAnswer: "Blue"
+    },
+    {
+        questionName: "What is your favorite color?",
+        answerOptions: [
+            "Red",
+            "Green",
+            "Yellow",
+            "Plad"
+        ],
+        correctAnswer: "Red"
+    },
+    {
+        questionName: "How many people have landed on the moon?",
+        answerOptions: [
+            "12",
+            "2",
+            "3",
+            "121"
+        ],
+        correctAnswer: "12"
+    },
+    {
+        questionName: "What color is the sky?",
+        answerOptions: [
+            "Blue",
+            "Red",
+            "Alert",
+            "Number"
+        ],
+        correctAnswer: "Blue"
+    },
+    {
+        questionName: "What is your favorite color?",
+        answerOptions: [
+            "Red",
+            "Green",
+            "Yellow",
+            "Plad"
+        ],
+        correctAnswer: "Red"
+    },
+    {
+        questionName: "How many people have landed on the moon?",
+        answerOptions: [
+            "12",
+            "2",
+            "3",
+            "11"
+        ],
+        correctAnswer: "12"
+    },
+    {   
+
+        questionName: "What color is the sky?",
+        answerOptions: [
+            "Blue",
+            "Red",
+            "Alert",
+            "Number"
+        ],
+        correctAnswer: "Blue"
+    },
+    {
+        questionName: "What is your favorite color?",
+        answerOptions: [
+            "Red",
+            "Green",
+            "Yellow",
+            "Plad"
+        ],
+        correctAnswer: "Red"
+    },
+    {
+        questionName: "How many people have landed on the moon?",
+        answerOptions: [
+            "12",
+            "2",
+            "3",
+            "11"
+        ],
+        correctAnswer: "12"
+    },
+    {
+        questionName: "What color is the sky?",
+        answerOptions: [
+            "Blue",
+            "Red",
+            "Alert",
+            "Number"
+        ],
+        correctAnswer: "Blue"
+    },
+    {
+        questionName: "What is your favorite color?",
+        answerOptions: [
+            "Red",
+            "Green",
+            "Yellow",
+            "Plad"
+        ],
+        correctAnswer: "Red"
+    },
+    {
+        questionName: "How many people have landed on the moon?",
+        answerOptions: [
+            "12",
+            "2",
+            "3",
+            "11"
+        ],
+        correctAnswer: "12"
+    },
+    {   
+
+        questionName: "What color is the sky?",
+        answerOptions: [
+            "Blue",
+            "Red",
+            "Alert",
+            "Number"
+        ],
+        correctAnswer: "Blue"
+    },
+    {
+        questionName: "What is your favorite color?",
+        answerOptions: [
+            "Red",
+            "Green",
+            "Yellow",
+            "Plad"
+        ],
+        correctAnswer: "Red"
+    },
+    {
+        questionName: "How many people have landed on the moon?",
+        answerOptions: [
+            "12",
+            "2",
+            "3",
+            "11"
+        ],
+        correctAnswer: "12"
+    },
+    {
+        questionName: "What color is the sky?",
+        answerOptions: [
+            "Blue",
+            "Red",
+            "Alert",
+            "Number"
+        ],
+        correctAnswer: "Blue"
+    },
+    {
+        questionName: "What is your favorite color?",
+        answerOptions: [
+            "Red",
+            "Green",
+            "Yellow",
+            "Plad"
+        ],
+        correctAnswer: "Red"
+    },
+    {
+        questionName: "How many people have landed on the moon?",
+        answerOptions: [
+            "12",
+            "2",
+            "3",
+            "1"
+        ],
+        correctAnswer: "12"
+    },
 ]
 
 
