704d7706c4143bb0ec598e8b7ff1e3e4bc7a90c0
chriskimball <92878250+chriskimball@users.noreply.github.com>
2021-11-24 23:27:53 -0800

    moving question bank list into its own js file for organization.

---
+++ assets/question_bank.js
@@ -0,0 +1,67 @@
+// Array of Questions
+var questions = [
+    {
+        questionName: "What color is the sky?",
+        answerOptions: [
+            "Blue",
+            "Red",
+            "Alert",
+            "Number"
+        ],
+        correctAnswer: "Blue"
+    },
+    {
+        questionName: "What is your favorite color?",
+        answerOptions: [
+            "Red",
+            "Green",
+            "Yellow",
+            "Plad"
+        ],
+        correctAnswer: "Red"
+    },
+    {
+        questionName: "How many people have landed on the moon?",
+        answerOptions: [
+            "1",
+            "2",
+            "3",
+            "12"
+        ],
+        correctAnswer: "12"
+    },
+    {
+        questionName: "What color is the sky?",
+        answerOptions: [
+            "Blue",
+            "Red",
+            "Alert",
+            "Number"
+        ],
+        correctAnswer: "Blue"
+    },
+    {
+        questionName: "What is your favorite color?",
+        answerOptions: [
+            "Red",
+            "Green",
+            "Yellow",
+            "Plad"
+        ],
+        correctAnswer: "Red"
+    },
+    {
+        questionName: "How many people have landed on the moon?",
+        answerOptions: [
+            "1",
+            "2",
+            "3",
+            "12"
+        ],
+        correctAnswer: "12"
+    }
+]
+
+
+
+window.questionBank = questions
