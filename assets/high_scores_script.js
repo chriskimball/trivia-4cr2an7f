be388320f81d96074f2e7601c115121b464b5f25
chriskimball <92878250+chriskimball@users.noreply.github.com>
2021-11-29 21:54:30 -0800

    Added comments to high scores script file

--- assets/high_scores_script.js
+++ assets/high_scores_script.js
@@ -1,16 +1,19 @@
+// Storing html elements from High_Scores page to modify them
 var leaderboardEl = document.getElementById('leaderboard')
 var backBtnEl = document.getElementById('backBtn')
 var clearBtnEl = document.getElementById('clearBtn')
 
+// Declaring variables for us to use
 var leaderboard = []
 var htmlTemplateString = "";
 
-// initial page load function
+// Initial page load function
 function init() {
 
-    // this will parse the todo array from local storage from string into an array
+    // This will parse the leaderboard array from local storage from string into an array
     leaderboard = JSON.parse(localStorage.getItem("leaderboard"));
     
+    // This will sort the leaderboard table by the total score
     leaderboard.sort(function(a, b){
         var scoreA= a.totalScore;
         var scoreB= b.totalScore;
@@ -20,6 +23,7 @@ function init() {
     renderLeaderboard()
 }
 
+// This funciton navigates the user back to the index.html if they click it and store the leaderboard into local storage
 function backToWelcome(event) {
     if (!event.target.matches("#backBtn")){
         return
@@ -29,12 +33,12 @@ function backToWelcome(event) {
     window.location.href='index.html'
 }
 
-// Renders leaderboard upon leaderboard page load or update
+// Renders leaderboard upon leaderboard page load or leaderboard update
 function renderLeaderboard() {
     htmlTemplateString = "";
     for(var i=0; i < leaderboard.length; i++) {
         
-        //back ticks for template literals is key
+        //Template literal which will print out the leaderboard for each object within the localstorage array
         htmlTemplateString += `
         <tr class="leaderboard-list">
         <td>${leaderboard[i].initials}</td>
@@ -45,14 +49,13 @@ function renderLeaderboard() {
     }
     leaderboardEl.innerHTML += htmlTemplateString
 
-    console.log(leaderboard)
+    // If the leaderboard is blank (cleared out or no scores logged yet), the table will be hidden
     if (leaderboard.length === 0) {
-        console.log(leaderboard)
         leaderboardEl.className= "hidden";
     } else {leaderboardEl.className= "displayed";}
 }
 
-// Clear out the leaderboard from local storage
+// Clear out the leaderboard from local storage and re-rendering the blank leaderboard to the page
 function clearLeaderboard () {
     leaderboard = []
     leaderboardEl.children[1].innerHTML = "";
@@ -61,8 +64,10 @@ function clearLeaderboard () {
     renderLeaderboard()  
 }
 
+// Calling the initial page load function
 init()
 
+// Event listeners for clicks on the two buttons on the page
 backBtnEl.addEventListener( "click" , backToWelcome )
 clearBtnEl.addEventListener( "click" , clearLeaderboard )
 
